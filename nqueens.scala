2f720575512504dc3fa5262a667597f3723e3cd8
Joe Tannenbaum <joseph.i.tannenbaum@gmail.com>
2018-10-09 10:10:01 -0400

    Initial scala script

---
+++ nqueens.scala
@@ -0,0 +1,26 @@
+#!/usr/bin/env scala
+
+import Math._
+
+case class Queen(x: Int, y: Int)
+
+class Solution(n: Int, queens: List[Queen]) {
+    override def toString: String = {
+        (0 to n - 1).map { i =>
+            ((0 to n - 1).map { j =>
+                if (queens contains Queen(i, j)) {
+                    "1 "
+                } else {
+                    "0 "
+                }
+            }).mkString("")
+        }.mkString("\n")
+    }
+}
+
+
+object NQueens extends App {
+    val testSolution = new Solution(4, List(Queen(1, 1), Queen(2, 2), Queen(3, 3), Queen(0, 1)))
+    println(testSolution);
+}
+NQueens.main(args)
