e47dadc2f8704efac5c29ca06b5cfc4b1c7de20d
Joe Tannenbaum <joseph.i.tannenbaum@gmail.com>
2018-10-09 13:39:37 -0400

    working implementation

--- nqueens.scala
+++ nqueens.scala
@@ -2,9 +2,19 @@
 
 import Math._
 
-case class Queen(x: Int, y: Int)
+trait Piece {
+    def nonattacking(other: Queen): Boolean
+}
 
-class Solution(queens: List[Queen]) {
+case class Queen(x: Int, y: Int) extends Piece {
+    def nonattacking(other: Queen): Boolean = {
+        x != other.x && y != other.y && abs( other.x - x ) != abs( other.y - y )
+    }
+}
+
+// A chessboard with some number of Queens pieces placed
+class Solution(_queens: List[Queen]) {
+    val queens = _queens
     val n = queens.length
     override def toString: String = {
         (0 to n - 1).map { i =>
@@ -19,5 +29,45 @@ class Solution(queens: List[Queen]) {
     }
 }
 
-val testSolution = new Solution(List(Queen(1, 1), Queen(2, 2), Queen(3, 3), Queen(0, 1)))
-println(testSolution);
+object NQueens {
+    // Chessboard has a pair of Queens attacking each other
+    def promising(solution: Solution): Boolean = {
+        solution.queens match {
+            case head +: tail => tail.forall(_ nonattacking head)
+        }
+    }
+
+    // Recursive backtracing to find a full Solution with n Queen pieces
+    def backtracking(n: Int, solution: Solution): Solution = {
+        val hd::tail = solution.queens
+        if (hd.x == n)
+            backtracking(n, new Solution(Queen(tail.head.x + 1, tail.head.y) +: tail.tail))
+        else if (promising(solution)) {
+            if (solution.queens.length == n)
+                solution
+            else
+                backtracking(n, new Solution(Queen(0, hd.y + 1) +: solution.queens))
+        }
+        else
+            backtracking(n, new Solution(Queen(hd.x + 1, hd.y) +: tail))
+    }
+}
+
+// toString
+val toStringSolution = new Solution(List(Queen(0, 0), Queen(1, 1)))
+assert("1 0 \n0 1 " == toStringSolution.toString)
+
+// nonattacking
+assert(Queen(1, 0) nonattacking Queen(2, 3))
+assert(!( Queen(1, 0) nonattacking Queen(0, 1) ))
+
+// promising
+val solutionThatIsPromising = new Solution(List(Queen(0, 0), Queen(2, 4)))
+val solutionThatIsNotPromising = new Solution(List(Queen(5, 5), Queen(2, 2)))
+assert(NQueens.promising(solutionThatIsPromising))
+assert(!NQueens.promising(solutionThatIsNotPromising))
+
+// finding a solution
+println(NQueens.backtracking(args(0).toInt, new Solution(List(Queen(0, 0)))))
+
+
